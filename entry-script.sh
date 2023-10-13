93a3b251a56061dc65816579c6f4ad09b20dd870
MD Mahbub Hasan <xmahbub@MDs-MacBook-Pro.local>
2023-10-13 06:11:02 +0600

    only docker

--- entry-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Set permissions on /var/www/html/www/admin/plugins
-echo "Current working directory: $(pwd)"
-chmod +x www/admin/plugins/
-chown -R www-data:www-data /var/www/html
-
-# Start Apache in the foreground
-exec /usr/sbin/apache2ctl -DFOREGROUND
